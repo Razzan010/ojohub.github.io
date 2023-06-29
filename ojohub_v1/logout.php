ac97b69092801c6bc69c39d2e5c1a2c38a8d79f7
Razzan010 <138133674+Razzan010@users.noreply.github.com>
2023-06-29 17:06:24 +0200

    updated

--- ojohub_v1/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-
-echo "Your are Logged out.";
-echo "<script>window.open('Home','_self')</script>"; 
- ?>
--- ojohub_v1/redirect_mobile.php
+++
@@ -1,7 +0,0 @@
- <?php 
-     if (isset($_SESSION['customer_email'])) {
-         
-     }else{
-         header('Location: http://www.store.ojohub.com/EnteringNow-m');
-     }
- ?>
